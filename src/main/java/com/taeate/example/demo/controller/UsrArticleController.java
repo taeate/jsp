f59da63d40b4cbb2dd2b58396d19997c056a68c6
taeate <yes3144@nate.com>
2022-02-07 20:55:28 +0900

    게시물 수정 기능 구현

--- src/main/java/com/taeate/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/taeate/example/demo/controller/UsrArticleController.java
@@ -56,6 +56,13 @@ public class UsrArticleController {
 
         articles.remove(article);
     }
+    private void ModifyArticle(int id, String title, String body) {
+        Article article = getArticle(id);
+
+        article.setTitle(title);
+        article.setBody(body);
+
+    }
 
     // 서비스 메서드 끝
 
@@ -79,6 +86,20 @@ public class UsrArticleController {
         deleteArticle(id);
         return id + "번 게시물을 삭제하였습니다.";
     }
+    
+    @RequestMapping("/usr/article/doModify")
+    @ResponseBody
+    public String doModify(int id, String title, String body) {
+        Article article = getArticle(id);
+
+        if( article == null){
+            return id + "번 게시물이 존재하지 않습니다.";
+        }
+        ModifyArticle(id, title, body);
+        return id + "번 게시물을 수정하였습니다.";
+    }
+
+
     //액션메서드 끝
     @RequestMapping("/usr/article/getArticles")
     @ResponseBody
