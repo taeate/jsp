c903ec63b7e6126624d5e419cf65388cd5b079bb
taeate <yes3144@nate.com>
2022-02-09 09:22:14 +0900

    게시물 상세보기 구현

--- src/main/java/com/taeate/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/taeate/example/demo/controller/UsrArticleController.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
 public class UsrArticleController {
     // 인스턴스 변수 시작
@@ -25,15 +24,16 @@ public class UsrArticleController {
     }
 
     private void makeTestData() {
-        for(int i =1; i <= 10; i++){
+        for (int i = 1; i <= 10; i++) {
 
             String title = "제목" + 1;
             String body = "내용" + 1;
-            writeArticle(title,body);
+            writeArticle(title, body);
         }
     }
+
     // 서비스 메서드 시작
-    public Article writeArticle(String title, String body){
+    public Article writeArticle(String title, String body) {
 
         int id = articlesLastId + 1;
         Article article = new Article(id, title, body);
@@ -43,19 +43,22 @@ public class UsrArticleController {
 
         return article;
     }
+
     private Article getArticle(int id) {
-        for(Article article : articles){
-            if( article.getId()==id){
+        for (Article article : articles) {
+            if (article.getId() == id) {
                 return article;
             }
         }
         return null;
     }
+
     private void deleteArticle(int id) {
         Article article = getArticle(id);
 
         articles.remove(article);
     }
+
     private void ModifyArticle(int id, String title, String body) {
         Article article = getArticle(id);
 
@@ -66,12 +69,12 @@ public class UsrArticleController {
 
     // 서비스 메서드 끝
 
-    //액션메서드 시작
+    // 액션메서드 시작
     @RequestMapping("/usr/article/doAdd")
     @ResponseBody
-    public Article doAdd(String title, String body){
-       
-       Article article = writeArticle(title,body);
+    public Article doAdd(String title, String body) {
+
+        Article article = writeArticle(title, body);
         return article;
     }
 
@@ -80,31 +83,41 @@ public class UsrArticleController {
     public String doDelete(int id) {
         Article article = getArticle(id);
 
-        if( article == null){
+        if (article == null) {
             return id + "번 게시물이 존재하지 않습니다.";
         }
         deleteArticle(id);
         return id + "번 게시물을 삭제하였습니다.";
     }
-    
+
     @RequestMapping("/usr/article/doModify")
     @ResponseBody
     public String doModify(int id, String title, String body) {
         Article article = getArticle(id);
 
-        if( article == null){
+        if (article == null) {
             return id + "번 게시물이 존재하지 않습니다.";
         }
         ModifyArticle(id, title, body);
         return id + "번 게시물을 수정하였습니다.";
     }
 
-
-    //액션메서드 끝
+    // 액션메서드 끝
     @RequestMapping("/usr/article/getArticles")
     @ResponseBody
     public List<Article> getArticles() {
         return articles;
     }
-    
+
+    @RequestMapping("/usr/article/getArticle")
+    @ResponseBody
+    public Object getArticleAction(int id) {
+        Article article = getArticle(id);
+
+        if (article == null) {
+            return id + "번 게시물은 존재하지 않습니다.";
+        }
+        return article;
+    }
+
 }
