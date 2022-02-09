677862efd41b85d370a4fcf2b2594dd58f893245
taeate <yes3144@nate.com>
2022-02-09 11:27:25 +0900

    ArticleSercive에 있던 단순 데이터 저장과 관련된 로직은 ArticleRepository 에 옮김

---
+++ src/main/java/com/taeate/example/demo/Repository/ArticleRepository.java
@@ -0,0 +1,71 @@
+package com.taeate.example.demo.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.taeate.example.demo.vo.Article;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArticleRepository {
+
+    private int articlesLastId;
+    private List<Article> articles;
+
+    public ArticleRepository() {
+        articlesLastId = 0;
+        articles = new ArrayList<>();
+
+        makeTestData();
+    }
+
+    public void makeTestData() {
+        for (int i = 1; i <= 10; i++) {
+
+            String title = "제목" + 1;
+            String body = "내용" + 1;
+            writeArticle(title, body);
+        }
+    }
+
+    // 서비스 메서드 시작
+    public Article writeArticle(String title, String body) {
+
+        int id = articlesLastId + 1;
+        Article article = new Article(id, title, body);
+
+        articles.add(article);
+        articlesLastId = id;
+
+        return article;
+    }
+
+    public Article getArticle(int id) {
+        for (Article article : articles) {
+            if (article.getId() == id) {
+                return article;
+            }
+        }
+        return null;
+    }
+
+    public void deleteArticle(int id) {
+        Article article = getArticle(id);
+
+        articles.remove(article);
+    }
+
+    public void ModifyArticle(int id, String title, String body) {
+        Article article = getArticle(id);
+
+        article.setTitle(title);
+        article.setBody(body);
+
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+}
--- src/main/java/com/taeate/example/demo/service/ArticleService.java
+++ src/main/java/com/taeate/example/demo/service/ArticleService.java
@@ -1,76 +1,35 @@
 package com.taeate.example.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import com.taeate.example.demo.Repository.ArticleRepository;
 import com.taeate.example.demo.vo.Article;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArticleService {
+    @Autowired
+    private ArticleRepository articleRepository;
 
-    // 인스턴스 변수 시작
-    private int articlesLastId;
-    private List<Article> articles;
-
-    // 인스턴스 변수 끝
-
-    public ArticleService() {
-        articlesLastId = 0;
-        articles = new ArrayList<>();
-
-        makeTestData();
-    }
-
-    public void makeTestData() {
-        for (int i = 1; i <= 10; i++) {
-
-            String title = "제목" + 1;
-            String body = "내용" + 1;
-            writeArticle(title, body);
-        }
-    }
-
-    // 서비스 메서드 시작
     public Article writeArticle(String title, String body) {
-
-        int id = articlesLastId + 1;
-        Article article = new Article(id, title, body);
-
-        articles.add(article);
-        articlesLastId = id;
-
-        return article;
+        return articleRepository.writeArticle(title, body);
     }
 
     public Article getArticle(int id) {
-        for (Article article : articles) {
-            if (article.getId() == id) {
-                return article;
-            }
-        }
-        return null;
-    }
-
-    public void deleteArticle(int id) {
-        Article article = getArticle(id);
-
-        articles.remove(article);
-    }
-
-    public void ModifyArticle(int id, String title, String body) {
-        Article article = getArticle(id);
-
-        article.setTitle(title);
-        article.setBody(body);
-
+        return articleRepository.getArticle(id);
     }
 
     public List<Article> getArticles() {
-        return articles;
+        return articleRepository.getArticles();
     }
 
-    // 서비스 메서드 끝
+    public void ModifyArticle(int id, String title, String body) {
+        articleRepository.ModifyArticle(id, title, body);
+    }
 
+    public void deleteArticle(int id) {
+        articleRepository.deleteArticle(id);
+    }
 }
