cf8b58a4d5930f1c6a8e7c1ae008723f7f969cd4
taeate <yes3144@nate.com>
2022-02-15 20:58:32 +0900

    게시물 테이블에 회원번호칼럼 추가

--- src/DB/schema.sql
+++ src/DB/schema.sql
@@ -78,3 +78,12 @@ loginPw = '1234',
 nickname = '사용자2',
 cellphoneNo = '01072396666',
 email = 'jjh1234@naver.com'
+
+# 게시물 테이블에 회원정보 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`
+DESC article;
+
+# 기존 게시물의 작성자를 2번 으로 지정
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
