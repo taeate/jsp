0770396ff5e744033c917d86e533346cc41b6712
taeate <yes3144@nate.com>
2022-02-22 23:19:07 +0900

    출력용 게시물 가져올 때, 액터가 해당 게시물을 삭제/수정 할 수 있는지도 파악

--- src/main/java/com/taeate/example/demo/service/ArticleService.java
+++ src/main/java/com/taeate/example/demo/service/ArticleService.java
@@ -26,18 +26,35 @@ public class ArticleService {
         return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), "id", id);
     }
 
-    public Article getForPrintArticle(int id) {
-        return articleRepository.getForPrintArticle(id);
-    }
+    public Article getForPrintArticle(int actorId, int id) {
+		Article article = articleRepository.getForPrintArticle(id);
 
-    public List<Article> getForPrintArticles() {
-        return articleRepository.getArticles();
-    }
+		updateForPrintData(actorId, article);
 
+		return article;
+	}
+    private void updateForPrintData(int actorId, Article article) {
+		if ( article == null ) {
+			return;
+		}
+
+		ResultData actorCanDeleteRd = actorCanDelete(actorId, article);
+		article.setExtra__actorCanDelete(actorCanDeleteRd.isSuccess());
+	}
+
+    public List<Article> getForPrintArticles(int actorId) {
+		List<Article> articles = articleRepository.getArticles();
+
+		for ( Article article : articles ) {
+			updateForPrintData(actorId, article);
+		}
+
+		return articles;
+	}
     public ResultData ModifyArticle(int id, String title, String body) {
         articleRepository.modifyArticle(id, title, body);
 
-        Article article = getForPrintArticle(id);
+        Article article = getForPrintArticle(0, id);
 
         return ResultData.from("S-1", Ut.f("%d번 게시물을 수정하였습니다.", id), "article", article);
     }
@@ -48,7 +65,7 @@ public class ArticleService {
 
     public ResultData actorCanModify(int actorId, Article article) {
         if (article == null){
-            return ResultData.from("F-1", "권한이 없습니다.");
+            return ResultData.from("F-1", "게시물이 존재하지않습니다.");
         }
 
         if (article.getMemberId() != actorId ){
@@ -56,4 +73,17 @@ public class ArticleService {
         }
     return ResultData.from("S-1", "수정가능합니다.");
     }
+
+
+    public ResultData actorCanDelete(int actorId, Article article) {
+		if ( article == null ) {
+			return ResultData.from("F-1", "게시물이 존재하지 않습니다.");
+		}
+
+		if ( article.getMemberId() != actorId ) {
+			return ResultData.from("F-2", "권한이 없습니다.");
+		}
+
+		return ResultData.from("S-1", "게시물 삭제가 가능합니다.");
+	}
 }
