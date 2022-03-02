5de23d7e14db85cf90d18ae4384570adf65652ea
taeate <yes3144@nate.com>
2022-03-02 11:17:44 +0900

    NeedLogin인터셉터 구현 및 등록

---
+++ src/main/java/com/taeate/example/demo/config/MyWebMvcConfigurer.java
@@ -0,0 +1,34 @@
+package com.taeate.example.demo.config;
+
+import com.taeate.example.demo.interceptor.BeforeActionInterceptor;
+import com.taeate.example.demo.interceptor.NeedLoginInterceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+    // beforeActionInterceptor 인터셉터 불러오기
+    @Autowired
+    BeforeActionInterceptor beforeActionInterceptor;
+    // NeedloginInterceptor 인터셉터 불러오기
+    @Autowired
+    NeedLoginInterceptor needLoginInterceptor;
+
+    // 이 함수는 인터셉터를 적용하는 역할
+    public void addInterceptors(InterceptorRegistry registry){
+         registry.addInterceptor(beforeActionInterceptor)
+                 .addPathPatterns("/**").excludePathPatterns("/resource/**")
+                 .excludePathPatterns("/error");
+
+
+        registry.addInterceptor(needLoginInterceptor)
+                 .addPathPatterns("/usr/article/write")
+                 .addPathPatterns("/usr/article/dowrite")
+                 .addPathPatterns("/usr/article/modify")
+                 .addPathPatterns("/usr/article/doModify")
+                 .addPathPatterns("/usr/article/doDelete");
+    }
+}
