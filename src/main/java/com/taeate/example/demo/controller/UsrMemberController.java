c2cbda51c6232f9a0af60f4f4f41e0c05b90a1ad
taeate <yes3144@nate.com>
2022-03-02 21:56:59 +0900

    로그인 처리에 Rq 적용

--- src/main/java/com/taeate/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/taeate/example/demo/controller/UsrMemberController.java
@@ -1,11 +1,13 @@
 package com.taeate.example.demo.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import com.taeate.example.demo.service.MemberService;
 import com.taeate.example.demo.util.Ut;
 import com.taeate.example.demo.vo.Member;
 import com.taeate.example.demo.vo.ResultData;
+import com.taeate.example.demo.vo.Rq;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -84,14 +86,10 @@ public class UsrMemberController {
 
     @RequestMapping("/usr/member/doLogin")
 	@ResponseBody
-	public String doLogin(HttpSession httpSession, String loginId, String loginPw) {
-		boolean isLogined = false;
+	public String doLogin(HttpServletRequest req, String loginId, String loginPw) {
+		Rq rq = (Rq)req.getAttribute("rq");
 
-		if (httpSession.getAttribute("loginedMemberId") != null) {
-			isLogined = true;
-		}
-
-		if (isLogined) {
+		if (rq.isLogined()) {
 			return Ut.jsHistoryBack("이미 로그인되었습니다.");
 		}
         if (Ut.empty(loginPw)) {
@@ -113,7 +111,8 @@ public class UsrMemberController {
         if (member.getLoginPw().equals(loginPw) == false ){
             return Ut.jsHistoryBack("비밀번호가 일치하지 않습니다.");
         }
-        httpSession.setAttribute("loginedMemberId", member.getId());
+        
+        rq.login(member);
 
         return Ut.jsReplace(Ut.f("%s님 환영합니다", member.getNickname()), "/");
         
