7d348a26dd8b736c1dd2fa97390e2f5c712f3f45
taeate <yes3144@nate.com>
2022-03-20 11:17:26 +0900

    회원정보 수정 전에, 비밀번호 체크시 인증코드 생성

--- src/main/java/com/taeate/example/demo/service/MemberService.java
+++ src/main/java/com/taeate/example/demo/service/MemberService.java
@@ -11,8 +11,10 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private MemberRepository memberRepository;
+    private AttrService attrService;
 
-    public MemberService(MemberRepository memberRepository){
+    public MemberService(AttrService attrService, MemberRepository memberRepository){
+        this.attrService = attrService;
         this.memberRepository = memberRepository;
     }
 
@@ -53,6 +55,14 @@ public class MemberService {
         memberRepository.modify(id,loginPw,name,nickname,email,cellphoneNo);
 
         return ResultData.from("S-1", "회원정보가 수정되었습니다");
+    }
+
+    public String genMemberModifyAuthKey(int actorId) {
+        String memberModifyAuthKey = Ut.getTempPassword(10);
+
+        attrService.setValue("member", actorId, "extra", "memberModifyAuthKey", memberModifyAuthKey, Ut.getDateStrLater(60 * 5));
+
+        return memberModifyAuthKey;
     }
 
 }
